37d9cb9b48d85315adf0a99e577c95bd87d8ff32
typicl <typicool@kingdomstudios.io>
2024-11-24 13:49:54 -0700

    feat: Create navigation component

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,21 +1,27 @@
+import { Link } from 'react-router-dom';
 import logo from '../../assets/multitech-logo.svg';
 import styles from './index.module.scss';
+import { ROUTES } from '../../constants/routes';
 
 export function Navigation() {
   const links = [
-    { label: 'Solutions (MPS)', url: '/solutions' },
-    { label: 'About', url: '/about' },
-    { label: 'Contact', url: '/contact' },
+    { label: 'Solutions (MPS)', url: ROUTES.SOLUTIONS },
+    { label: 'About', url: ROUTES.ABOUT },
+    { label: 'Contact', url: ROUTES.CONTACT },
   ];
   return (
     <nav className={styles.navigation}>
-      <img className={styles.logo} src={logo} />
       <div>
-        {links.map(l => (
-          <a key={l.label} href={l.url}>
-            {l.label}
-          </a>
-        ))}
+        <Link className={styles.logo} to={ROUTES.HOME}>
+          <img src={logo} />
+        </Link>
+        <div>
+          {links.map(l => (
+            <a key={l.label} href={l.url}>
+              {l.label}
+            </a>
+          ))}
+        </div>
       </div>
     </nav>
   );
