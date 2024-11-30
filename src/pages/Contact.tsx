5eb708b694bb81437d927d45dfa5975b0d348408
typicl <typicool@kingdomstudios.io>
2024-11-30 12:37:22 -0700

    feat: Add new Contact page and rework styles to fit reusable page styles

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,26 +1,124 @@
-import { Button } from '../components/button';
+import { ChangeEvent, useState } from 'react';
 import { Footer } from '../components/footer';
-import { ROUTES } from '../constants/routes';
+import styles from './page.module.scss';
+import ReCAPTCHA from 'react-google-recaptcha';
+
+type InputEvent = ChangeEvent<HTMLInputElement>;
 
 export function Contact() {
+  const [name, setName] = useState('');
+  function handleNameChange(e: InputEvent) {
+    setName(e.target.value);
+  }
+  const [phone, setPhone] = useState('');
+  function handlePhoneChange(e: InputEvent) {
+    setPhone(e.target.value);
+  }
+  const [email, setEmail] = useState('');
+  function handleEmailChange(e: InputEvent) {
+    setEmail(e.target.value);
+  }
+  const [message, setMessage] = useState('');
+  function handleMessageChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setMessage(e.target.value);
+  }
+  const [validated, setValidated] = useState(false);
+  function handleCaptchaSuccess() {
+    setValidated(true);
+  }
+  function handleCaptchaExpired() {
+    setValidated(false);
+  }
   return (
     <div>
-      <section>
-        <h1>Contact Us</h1>
-        <h2>Start saving time and money today</h2>
-        <p>
-          As the largest independent office equipment dealer in the Gulf Coast
-          area of Louisiana and Mississippi, we specialize in third party
-          printer, copier, fax, and multi-function device maintenance.
-        </p>
-        <div>
-          <Button to={ROUTES.CONTACT}>Contact us</Button>
-          <Button to={ROUTES.SOLUTIONS} useOutline>
-            Learn about our solutions
-          </Button>
-        </div>
-      </section>
+      <main className={styles.page}>
+        <section className={styles.section}>
+          <h2>Contact Us</h2>
+          <h3>Start saving time and money today</h3>
+          <p>
+            We work hand in hand with you to support your growth. As your
+            technology partner, our reward comes from your success and your
+            growth.
+          </p>
+        </section>
+        <section className={styles.section}>
+          <div className={styles.contactGroup}>
+            <div>
+              <h5>Send us a message</h5>
+              <p>
+                Send a message to our team and we'll get back ni touch within 2
+                business days.
+              </p>
 
+              <form>
+                <div>
+                  <label htmlFor="name">Name</label>
+                  <input
+                    id="name"
+                    type="text"
+                    value={name}
+                    onChange={handleNameChange}
+                    placeholder="First and last name"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    id="phone"
+                    type="tel"
+                    value={phone}
+                    onChange={handlePhoneChange}
+                    placeholder="(123) 123-4567"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    placeholder="email@domain.com"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="message">Message (optional)</label>
+                  <textarea
+                    id="message"
+                    value={message}
+                    onChange={handleMessageChange}
+                    placeholder="Add your message here..."
+                  />
+                </div>
+                <ReCAPTCHA
+                  sitekey="6Lfpvo4qAAAAALsZfz7sBy6BPSXH-L3z11WjYLW7"
+                  onChange={handleCaptchaSuccess}
+                  onExpired={handleCaptchaExpired}
+                />
+                <div className={styles.buttonWrapper}>
+                  <button type="submit" disabled={!validated}>
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className={styles.details}>
+              <div>
+                <h5>Email</h5>
+                <p>Send us an email at:</p>
+                <a href="mailto:email@multitechofficesupplies.com">
+                  email@multitechofficesupplies.com
+                </a>
+              </div>
+              <div>
+                <h5>Phone</h5>
+                <p>Call us at:</p>
+                <a href="tel:1-800-641-5291">1-800-641-5291</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,158 +29,161 @@ export function Home() {
 
   return (
     <div>
-      <section className={styles.hero}>
-        <div className={styles.content}>
-          <h2 className={styles.title}>
-            All of your office equipment management handled in one place &mdash;
-            none of the hassle.
-          </h2>
-          <p className={styles.paragraph}>
-            As the largest independent office equipment dealer in the Gulf Coast
-            area of Louisiana and Mississippi, we specialize in third party
-            printer, copier, fax, and multi-function device maintenance.
+      <div className={styles.page}>
+        <section className={styles.hero}>
+          <div className={styles.content}>
+            <h2 className={styles.title}>
+              All of your office equipment management handled in one place
+              &mdash; none of the hassle.
+            </h2>
+            <p className={styles.paragraph}>
+              As the largest independent office equipment dealer in the Gulf
+              Coast area of Louisiana and Mississippi, we specialize in third
+              party printer, copier, fax, and multi-function device maintenance.
+            </p>
+            <div className={styles.buttonWrapper}>
+              <Button to={ROUTES.CONTACT}>Contact us</Button>
+              <Button to={ROUTES.SOLUTIONS} useOutline>
+                Learn about our solutions
+              </Button>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.section}>
+          <h2>Multi-print Solutions</h2>
+          <h3>How we can help</h3>
+          <p>
+            By streamlining the flow of information through all of your business
+            processes, our services can help increase overall productivity. Our
+            mission is a simple one: Helping you make your office technology an
+            asset for your business.
           </p>
-          <div className={styles.buttonWrapper}>
-            <Button to={ROUTES.CONTACT}>Contact us</Button>
-            <Button to={ROUTES.SOLUTIONS} useOutline>
-              Learn about our solutions
+          <TabsSection tabs={tabs} />
+          <Button to={ROUTES.SOLUTIONS}>Learn more about our solutions</Button>
+        </section>
+
+        <section className={cx(styles.section, styles.outline)}>
+          <div className={styles.center}>
+            <h2>What our customers love:</h2>
+            <h3>Heading callout for recycling</h3>
+          </div>
+          <div>
+            <p>
+              Brief description of offer and benefits. Why customers choose this
+              option and what it does for them.
+            </p>
+          </div>
+          <div className={styles.center}>
+            <Button to={ROUTES.SOLUTIONS} useOutlineDark>
+              See all of our services
             </Button>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className={styles.section}>
-        <h2>Multi-print Solutions</h2>
-        <h3>How we can help</h3>
-        <p>
-          By streamlining the flow of information through all of your business
-          processes, our services can help increase overall productivity. Our
-          mission is a simple one: Helping you make your office technology an
-          asset for your business.
-        </p>
-        <TabsSection tabs={tabs} />
-        <Button to={ROUTES.SOLUTIONS}>Learn more about our solutions</Button>
-      </section>
-
-      <section className={cx(styles.section, styles.outline)}>
-        <div className={styles.center}>
-          <h2>What our customers love:</h2>
-          <h3>Heading callout for recycling</h3>
-        </div>
-        <div>
+        <section className={styles.section}>
+          <h2>Quality assessments</h2>
+          <h3>Increase your Productivity</h3>
           <p>
-            Brief description of offer and benefits. Why customers choose this
-            option and what it does for them.
+            Understanding how your business currently manages its print devices
+            is the first step towards increased productivity. Our goal is to
+            provide you and your employees with the right business tools
+            allowing us to give your business maximum flexibility at the best
+            possible price.
           </p>
-        </div>
-        <div className={styles.center}>
-          <Button to={ROUTES.SOLUTIONS} useOutlineDark>
-            See all of our services
+        </section>
+
+        <section
+          className={cx(
+            styles.commitment,
+            styles.section,
+            styles.alt,
+            styles.center
+          )}
+        >
+          <h2>Our commitment</h2>
+          <h3>Working with us</h3>
+          <div>
+            <div>
+              <FontAwesomeIcon icon={faPrint} />
+              <h4>Simplified Printing</h4>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faAward} />
+              <h4>Guaranteed Quality</h4>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faFaceSmile} />
+              <h4>Free Maintenance</h4>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faHeadphones} />
+              <h4>Full Support</h4>
+            </div>
+          </div>
+          <Button to={ROUTES.ABOUT} useOutlineDark>
+            About our company
           </Button>
-        </div>
-      </section>
+        </section>
 
-      <section className={styles.section}>
-        <h2>Quality assessments</h2>
-        <h3>Increase your Productivity</h3>
-        <p>
-          Understanding how your business currently manages its print devices is
-          the first step towards increased productivity. Our goal is to provide
-          you and your employees with the right business tools allowing us to
-          give your business maximum flexibility at the best possible price.
-        </p>
-      </section>
-
-      <section
-        className={cx(
-          styles.commitment,
-          styles.section,
-          styles.alt,
-          styles.center
-        )}
-      >
-        <h2>Our commitment</h2>
-        <h3>Working with us</h3>
-        <div>
-          <div>
-            <FontAwesomeIcon icon={faPrint} />
-            <h4>Simplified Printing</h4>
+        <section className={cx(styles.printers, styles.section)}>
+          <div className={styles.center}>
+            <h2>Printers and Multifunction devices</h2>
+            <h3>Our machines and capabilities</h3>
           </div>
-          <div>
-            <FontAwesomeIcon icon={faAward} />
-            <h4>Guaranteed Quality</h4>
+          <div className={styles.half}>
+            <div>
+              <h5>Multifunction Devices</h5>
+              <p>
+                Understanding how your business currently manages its print
+                devices is the first step towards increased productivity. Our
+                goal is to provide you and your employees with the right
+                business tools allowing us to give your business maximum
+                flexibility at the best possible price.
+              </p>
+            </div>
+            <div>
+              <h5>Desktop Printers</h5>
+              <p>
+                We can help you determine not only what kind of printing device
+                you need, but also which printer will work best for your
+                business to ensure you are always getting the most reliable and
+                affordable machine available. By working with you to evaluate
+                the answers to these questions, we can provide printing
+                solutions that are optimized for your functional needs, volume
+                and budget. Call us today to start your free personalized
+                assessment.
+              </p>
+            </div>
           </div>
-          <div>
-            <FontAwesomeIcon icon={faFaceSmile} />
-            <h4>Free Maintenance</h4>
+          <div className={styles.half}>
+            <div>
+              <h5>New &amp; Refurbished Devices</h5>
+              <p>
+                When you partner with Multitech, you won't only be receiving the
+                best service, you will also be receiving the best devices too!
+                We offer a large range of new and refurbished HP printers and
+                MFPs that are certain to fulfill your needs.
+              </p>
+            </div>
+            <div>
+              <h5>Assessments &amp; Technology</h5>
+              <p>
+                Keeping your office running smooth is no challenge with our line
+                of HP printers and MFPs. Easily access, share, and print
+                documents using integrated technology such as HP ePrint and
+                Google Cloud Print services. We can help you identify challenges
+                and opportunities in managing your print environment and provide
+                you with practical methods to improve efficiency and
+                cost-effectiveness with our free remote monitoring service.
+              </p>
+            </div>
           </div>
-          <div>
-            <FontAwesomeIcon icon={faHeadphones} />
-            <h4>Full Support</h4>
+          <div className={cx(styles.center, styles.buttonWrapper)}>
+            <Button to={ROUTES.CONTACT}>Contact us for an assessment</Button>
           </div>
-        </div>
-        <Button to={ROUTES.ABOUT} useOutlineDark>
-          About our company
-        </Button>
-      </section>
-
-      <section className={cx(styles.printers, styles.section)}>
-        <div className={styles.center}>
-          <h2>Printers and Multifunction devices</h2>
-          <h3>Our machines and capabilities</h3>
-        </div>
-        <div className={styles.half}>
-          <div>
-            <h5>Multifunction Devices</h5>
-            <p>
-              Understanding how your business currently manages its print
-              devices is the first step towards increased productivity. Our goal
-              is to provide you and your employees with the right business tools
-              allowing us to give your business maximum flexibility at the best
-              possible price.
-            </p>
-          </div>
-          <div>
-            <h5>Desktop Printers</h5>
-            <p>
-              We can help you determine not only what kind of printing device
-              you need, but also which printer will work best for your business
-              to ensure you are always getting the most reliable and affordable
-              machine available. By working with you to evaluate the answers to
-              these questions, we can provide printing solutions that are
-              optimized for your functional needs, volume and budget. Call us
-              today to start your free personalized assessment.
-            </p>
-          </div>
-        </div>
-        <div className={styles.half}>
-          <div>
-            <h5>New &amp; Refurbished Devices</h5>
-            <p>
-              When you partner with Multitech, you won't only be receiving the
-              best service, you will also be receiving the best devices too! We
-              offer a large range of new and refurbished HP printers and MFPs
-              that are certain to fulfill your needs.
-            </p>
-          </div>
-          <div>
-            <h5>Assessments &amp; Technology</h5>
-            <p>
-              Keeping your office running smooth is no challenge with our line
-              of HP printers and MFPs. Easily access, share, and print documents
-              using integrated technology such as HP ePrint and Google Cloud
-              Print services. We can help you identify challenges and
-              opportunities in managing your print environment and provide you
-              with practical methods to improve efficiency and
-              cost-effectiveness with our free remote monitoring service.
-            </p>
-          </div>
-        </div>
-        <div className={cx(styles.center, styles.buttonWrapper)}>
-          <Button to={ROUTES.CONTACT}>Contact us for an assessment</Button>
-        </div>
-      </section>
-
+        </section>
+      </div>
       <Footer />
     </div>
   );
