5222fdbac07fe8c2a06bcc6e64721356130a7592
typicl <typicool@kingdomstudios.io>
2025-02-05 19:25:30 -0700

    fix: Update vite and setup new build items

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import type { UserConfig } from 'vite';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
@@ -6,46 +7,44 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => {
-  return {
-    base: '/multitech-website/',
-    css: {
-      preprocessorOptions: {
-        scss: {
-          includePaths: ['src'],
-          silenceDeprecations: ['import', 'legacy-js-api'],
-          additionalData: `
+export default defineConfig({
+  base: './',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['src'],
+        silenceDeprecations: ['import', 'legacy-js-api'],
+        additionalData: `
           @use 'sass:color';
-          @import "./src/styles/_base-refs.scss";
+          @import "/src/styles/_base-refs.scss";
           `,
-        },
       },
     },
-    optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: 'globalThis',
-        },
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-            process: true,
-          }),
-        ],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
       },
-      include: [],
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
     },
-    plugins: [
-      ViteImageOptimizer(),
-      nodePolyfills({
-        include: ['http', 'https'],
-      }),
-      react(),
-      svgr(),
-      tsconfigPaths(),
-    ],
-    resolve: {
-      extensions: ['.js', '.ts', '.jsx', '.tsx'],
-    },
-  };
-});
+    include: [],
+  },
+  plugins: [
+    ViteImageOptimizer(),
+    nodePolyfills({
+      include: ['http', 'https'],
+    }),
+    react(),
+    svgr(),
+    tsconfigPaths(),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+  },
+} satisfies UserConfig);
